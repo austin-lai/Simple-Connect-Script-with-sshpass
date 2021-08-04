970c6f7cbf53959ae26c4bb1d6be8b06585415dc
Austin.Lai <51234556+austin-lai@users.noreply.github.com>
2021-08-04 21:36:22 +0800

    Update connect-script-v0.2.sh

--- connect-script-v0.2.sh
+++ connect-script-v0.2.sh
@@ -2,16 +2,13 @@
 
 # Display Help or Usage
 display_usage() { 
-	echo -e "\nThis script to decode rot13 encoded text ...\n" 
-    echo -e "./decode-rot13-v0.2.sh -t wsadxre -o output-decoded-rot13.txt \n"
-	echo -e "Usage: $0 [ -h | -t | -o ]\n" 
-	echo -e "\t-h , --help"
-	echo -e "\t-t <encoded text>"
-	echo -e "\t-o <output filename>"
+	echo -e "\nThis script to ssh to target with sshpass ...\n" 
+    echo -e "./connect-script-v0.2.sh "test" 22222 test 192.168.0.100\n"
+	echo -e "Usage: $0 <'password' | \"password\"> <target ssh port> <target ssh username> <target ip>\n" 
 } 
 
 # If less than one arguments supplied, display usage 
-if [  $# -lt 1 ] 
+if [  $# -lt 4 ] 
 then 
 	display_usage
 	exit 1
@@ -25,46 +22,15 @@ then
 fi 
 
 
-# Check and get the parameter or option pass to the Argument
-while getopts "t:o:" opt
-do
-   case "$opt" in
-      t ) parameterA="$OPTARG" ;;
-      o ) parameterB="$OPTARG" ;;
-   esac
-done
+# Set your variable
+password=$1
+target_ssh_port=$2
+target_ssh_username=$3
+target_ipaddr=$4
 
+echo -e "\nPassword = $password"
+echo "Target SSH Port = $target_ssh_port"
+echo "Target Username = $target_ssh_username"
+echo -e "Target SSH IP = $target_ipaddr \n"
 
-# Print display_usage function in case parameters are empty  || [ -z "$parameterB" ]
-if [ -z "$parameterA" ] 
-then
-   echo -e "\nSome or all of the parameters are empty";
-   display_usage
-else
-    # Begin script in case all parameters are correct
-
-	# Store given encoded rot13 text from Argument 1 to variable encoded_text
-	encoded_text=$parameterA
-	echo -e "\nOptions -t = $encoded_text"
-
-    # Decode the rot13
-	decoded_rot13=$(echo -n "$encoded_text" | rot13)
-    printf "Encoded rot13 = $encoded_text, decoded rot13 = $decoded_rot13"
-
-    # Check if output file parameter is set?
-    if [ -z "$parameterB" ]
-    then
-
-        # Output file parameter not set, use default outputfile
-        echo -e "\nOptions -o not set, use default output file\n"
-
-        echo -e "$decoded_rot13 \n" >> decoded-rot13.txt
-    else
-        # Store given output filename from Argument 2 to variable output_file
-        output_file=$parameterB
-        echo -e "\nOptions -o = $output_file\n"
-
-        # Output the decoded rot13 to file as specify
-        echo -e "$decoded_rot13 \n" >> $output_file.txt
-    fi
-fi
+sshpass -v -p $password ssh -v -o StrictHostKeyChecking=no -p $target_ssh_port $target_ssh_username@$target_ipaddr
